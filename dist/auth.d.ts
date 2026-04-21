1f6c6f21c145830926a6486220c03d79ee740fd8
Samuel <samuel.zentou@my-digital-school.org>
2026-04-21 10:55:51 +0200

    chore: remove dist from tracking and update gitignore

--- dist/auth.d.ts
+++
@@ -1,5 +0,0 @@
-import type { AuthResponse } from "@supabase/supabase-js";
-export declare function signUp(email: string, password: string, username: string, fullName: string): Promise<AuthResponse["data"]>;
-export declare function signIn(email: string, password: string): Promise<AuthResponse["data"]>;
-export declare function signOut(): Promise<void>;
-//# sourceMappingURL=auth.d.ts.map
